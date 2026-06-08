99d8b9db15b03179ed4b42c093d2bd10c79ce794
Pitonsepia <m3lihs3mih@gmail.com>
2026-06-08 16:26:24 +0300

    Fix name, sounds, icons, UI and BT remote

--- android/app/src/main/java/com/timerlock/app/MainActivity.java
+++ android/app/src/main/java/com/timerlock/app/MainActivity.java
@@ -1,7 +1,11 @@
 package com.timerlock.app;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.view.KeyEvent;
+import android.view.View;
+import android.view.WindowInsets;
+import android.view.WindowInsetsController;
 import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
@@ -9,19 +13,63 @@ public class MainActivity extends BridgeActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        hideSystemUI();
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            hideSystemUI();
+        }
+    }
+
+    private void hideSystemUI() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            WindowInsetsController controller = getWindow().getInsetsController();
+            if (controller != null) {
+                controller.hide(WindowInsets.Type.statusBars() | WindowInsets.Type.navigationBars());
+                controller.setSystemBarsBehavior(
+                    WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+                );
+            }
+        } else {
+            getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+            );
+        }
+    }
+
+    private void sendWakeupToJS() {
+        getBridge().getWebView().post(() ->
+            getBridge().getWebView().evaluateJavascript(
+                "window.dispatchEvent(new KeyboardEvent('keydown', {key: 'ArrowUp', bubbles: true}));",
+                null
+            )
+        );
     }
 
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
-        // Volume Up key triggers the lock screen wakeup
-        if (keyCode == KeyEvent.KEYCODE_VOLUME_UP) {
-            // Inject ArrowUp keydown event into the WebView
-            getBridge().getWebView().evaluateJavascript(
-                "window.dispatchEvent(new KeyboardEvent('keydown', {key: 'ArrowUp', bubbles: true}));",
-                null
-            );
-            return true; // consume the event (don't change system volume)
+        switch (keyCode) {
+            // Hardware volume up
+            case KeyEvent.KEYCODE_VOLUME_UP:
+            // BT remotes - common keycodes
+            case KeyEvent.KEYCODE_PAGE_UP:
+            case KeyEvent.KEYCODE_DPAD_UP:
+            case KeyEvent.KEYCODE_MEDIA_NEXT:
+            case KeyEvent.KEYCODE_BUTTON_R1:
+            case KeyEvent.KEYCODE_BUTTON_A:
+                sendWakeupToJS();
+                return true; // consume event
+
+            default:
+                return super.onKeyDown(keyCode, event);
         }
-        return super.onKeyDown(keyCode, event);
     }
 }
